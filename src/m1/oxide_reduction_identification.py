a3bef9cbe94b4d2863b7601115d84ee24b066ad8
AndyDanian <danianescobarv@gmail.com>
2024-04-17 09:29:07 -0300

    improve the oxide_reduction_identification.py module

--- src/m1/oxide_reduction_identification.py
+++ src/m1/oxide_reduction_identification.py
@@ -15,7 +15,7 @@ Process:
     - Electronic movement
     - Classify the substance in reducing and oxidizing agents
 
-Notes (future edition or tasks):
+NOTE (future editions or tasks):
 
     - Oxidation number: is a virtual charge belong to an atom whether every
       bonds are ionic
@@ -39,8 +39,7 @@ Notes (future edition or tasks):
 from atomic_mass_percentage import split_symbols_amount
 
 
-def input_substance() -> tuple[str, int, str, int,
-                               str, int, str, int]:
+def input_substance() -> tuple[str, int, str, int, str, int, str, int]:
     """
     Input information from the prompt:
         - Reactives and charges
@@ -72,7 +71,7 @@ def input_substance() -> tuple[str, int, str, int,
 
 def input_oxidation_number(
     molecule_composition: dict[str, int], molecule: str, charge: int
-        ) -> dict[str, int]:
+) -> dict[str, int]:
     """
     Input oxidation number
     """
@@ -91,12 +90,12 @@ def input_oxidation_number(
 
 
 def calculate_oxidation_number(
-    molecule_composition: dict[str, int], oxidation_number: dict[str, int],
-        charge: int) -> dict[str, int]:
+    molecule_composition: dict[str, int], oxidation_number: dict[str, int], charge: int
+) -> dict[str, int]:
     """
     Oxidation number by each atom is calcularet if the atom don't have
     """
-    
+
     for atom, no in oxidation_number.items():
         if no == 999:
             oxidation_number[atom] = charge - sum(
@@ -114,9 +113,10 @@ def calculate_oxidation_number(
 
 def oxide_reduction(
     react: dict[str, int], prod: dict[str, int], mol_a: str, mol_b: str
-        ) -> dict[str, int]:
+) -> dict[str, int]:
     """
-    Find the oxidiz or reduction atoms
+    Classify the substances according like the electrons moving in the
+    reaction
     """
 
     oxidation_numbers: dict[str, int] = {}
@@ -143,20 +143,23 @@ def oxide_reduction(
 
 
 if __name__ == "__main__":
-    reactive_a: str = "HNO3"
-    reactive_b: str = "Sn"
-    product_a: str = "H2SnO3"
-    product_b: str = "NO"
+    # Example of inputs
+    reactive_a = "HNO3"
+    reactive_b = "Sn"
+    product_a = "H2SnO3"
+    product_b = "NO"
     r_charge_a = r_charge_b = p_charge_a = p_charge_b = 0
 
+    # Amount: each one the atoms
     comp_react_a = split_symbols_amount(reactive_a)
     comp_react_b = split_symbols_amount(reactive_b)
     comp_prod_a = split_symbols_amount(product_a)
     comp_prod_b = split_symbols_amount(product_b)
 
-    print(
-        "if you unknown the oxidation number, please input 999 and the program calculate it (only maximum one atom by molecule)"
-    )
+    # Oxidation number (input/calculations)
+    print("if you unknown the oxidation number, please input 999 and", end="")
+    print(" the program calculate it (only maximum one atom by molecule)")
+
     print("\n", "*" * 60, "\n")
     num_oxid_atoms_react_a = input_oxidation_number(
         comp_react_a, reactive_a, r_charge_a
@@ -166,11 +169,15 @@ if __name__ == "__main__":
     )
     num_oxid_atoms_prod_a = input_oxidation_number(comp_prod_a, product_a, p_charge_a)
     num_oxid_atoms_prod_b = input_oxidation_number(comp_prod_b, product_b, p_charge_b)
+
+    # Output oxidation number
     print("\n", "*" * 60, "\n")
     print(f"Number of oxidation of atoms in the {reactive_a}: {num_oxid_atoms_react_a}")
     print(f"Number of oxidation of atoms in the {reactive_b}: {num_oxid_atoms_react_b}")
     print(f"Number of oxidation of atoms in the {product_a}: {num_oxid_atoms_prod_a}")
     print(f"Number of oxidation of atoms in the {product_b}: {num_oxid_atoms_prod_b}")
+
+    # Output classification of substances
     print("\n", "*" * 60, "\n")
     nums_oxid_a_a = oxide_reduction(
         num_oxid_atoms_react_a, num_oxid_atoms_prod_a, reactive_a, product_a
