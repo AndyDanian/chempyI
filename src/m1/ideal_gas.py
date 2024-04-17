406a3b5a8f329984faf6e28a198ba38525ec27ca
AndyDanian <danianescobarv@gmail.com>
2024-04-17 00:38:06 -0300

    improve docstring/comment of ideal_gas.py

--- src/m1/ideal_gas.py
+++ src/m1/ideal_gas.py
@@ -1,41 +1,57 @@
-import constants
-from atomic_mass_percentage import split_symbols_amount, molecular_mass
-
 """
 Calculate the mass, volumen, temperature or presion of
-an ideal gas, from the equation:
+ideal gases, from the equation:
 
     v = m/MRT/P;  n = m/M*N
 
-v [L]      : volumen
-m [g]      : mass
-M [g/mol]  : molar mass
-T [kelvin] : temperature
-P [atm]    : presion
+v [L]             : volumen
+m [g]             : mass
+M [g/mol]         : molar mass
+T [kelvin]        : temperature
+P [atm]           : presion
 R [L atm/(k mol)] : Molar gas constant
-n []       : number of molecules
-N [mol]    : Avogadro number
+n                 : number of molecules
+N [mol]           : Avogadro number
 """
 
+import constants
+from atomic_mass_percentage import split_symbols_amount, molecular_mass
+
+
 # Constants
-R = constants.constants["R"]
-N = constants.constants["N"]
+R: float = constants.constants["R"]
+N: float = constants.constants["N"]
+
 
 # Input by user
 def read_temperature():
+    "Read temperature from prompt"
     return float(input("temperature in kelvin of the ideal gas: "))
+
+
 def read_mass():
+    "Read mass from prompt"
     return float(input("massa in g of the ideal gas: "))
+
+
 def read_presion():
+    "Read presion from prompt"
     return float(input("presion in atm of the ideal gas: "))
+
+
 def read_volumen():
+    "Read volumen from prompt"
     return float(input("volumen in L of the ideal gas: "))
 
-# Calculate
-def calculate_amount(calculate: str, molar_mass: float):
+
+def calculate_amount(
+    calculate: str, molar_mass: float
+) -> tuple[str, float, str, float]:
     """
-    Calculate the amount ask and also, return its units
+    Calculate the amount of unknown amount. Return its
+    value with the respective units
     """
+
     if calculate.lower != "t":
         T: float = read_temperature()
     if calculate.lower() != "p":
@@ -45,35 +61,41 @@ def calculate_amount(calculate: str, molar_mass: float):
     if calculate.lower() != "v":
         v: float = read_volumen()
     if calculate.lower() == "v":
-        message = "the volumen"
-        amount = m/molar_mass*T*R/P
-        unit = "L"
+        message: str = "volumen"
+        amount: float = m / molar_mass * T * R / P
+        unit: str = "L"
     elif calculate.lower() == "p":
-        message = "the presion"
-        amount = m/molar_mass*T*R/v
+        message = "presion"
+        amount = m / molar_mass * T * R / v
         unit = "atm"
     elif calculate.lower() == "t":
-        message = "the temperature"
-        amount = v*molar_mass/m*P/R
+        message = "temperature"
+        amount = v * molar_mass / m * P / R
         unit = "k"
     else:
-        message = "the mass"
-        amount = v*molar_mass*P/(T*R)
-        unit = "g"    
+        message = "mass"
+        amount = v * molar_mass * P / (T * R)
+        unit = "g"
+
+    # Massa
     if calculate.lower() == "m":
         m = amount
-    nmolecules: float = m/molar_mass*N
+
+    # Number of molecules
+    nmolecules: float = m / molar_mass * N
+
     return message, amount, unit, nmolecules
 
-# 
-formule = input("what is the chemistry formule of the ideal gas?")
-molar_mass = molecular_mass(split_symbols_amount(formule))
 
-calculate = input("what amount does it calculate [v/m/T/P]?")
+# Inputs
+formule: str = input("what is the chemistry formule of the ideal gas?")
+molar_mass: float = molecular_mass(split_symbols_amount(formule))
+
+calculate: str = input("what amount does it calculate [v/m/T/P]?")
 
 # Calculate
-message, amount, unit, nmolecules = calculate_amount(calculate,molar_mass)
+message, amount, unit, nmolecules = calculate_amount(calculate, molar_mass)
 
 # Print results
-print(f"{message} in the ideal gas is the {amount} {unit}")
-print(f"amount the molecules: {nmolecules} of {formule}")
+print(f"\nThe {message} in the ideal gas is the {amount} {unit}")
+print(f"Molecules amount {formule} is {nmolecules}")
