cc2640c6cf14080102694a97a378978d9041c712
AndyDanian <danianescobarv@gmail.com>
2024-04-17 09:52:04 -0300

    improve commets in sign_number_intensities_spin_spin.py

--- src/m1/sign_number_intesisties_spin_spin.py
+++ src/m1/sign_number_intesisties_spin_spin.py
@@ -1,32 +1,51 @@
 """
 Module 1 - Problem 8: Signs number and intesities relations
 
-This program determine the number of signs and their relations in a spin-spin coupling spectrum with the Pascal's triangle.
+This program determine the number of signs and their relations
+in a spin-spin coupling spectrum with the Pascal's triangle.
 
-    * Input number of nucleus equivalents
-    * Use the factorial function which is a recurrence to calculation 
+Methodology:
+    - Input number of nucleus equivalents
+    - Use the factorial function which is a recurrence to calculation
 """
 
+
 def factorial(number: int) -> int:
+    """
+    Drive the factorial calculation
+    """
 
     if number == 0 or number == 1:
         return 1
     else:
-        return number * factorial(number-1)
+        return number * factorial(number - 1)
 
-def spin_spin_sign(atoms_number: int) -> list:
+
+def spin_spin_sign(atoms_number: int) -> list[int]:
+    """
+    Calculate of number of signs
+    """
 
     if atoms_number == 1:
         return [1]
     else:
         list_sign: list[int] = [1]
         for n in range(atoms_number - 2):
-            list_sign.append(factorial(atoms_number-1)/(factorial(atoms_number-(n+2))*factorial(n+1)))
+            list_sign.append(
+                int(
+                    factorial(atoms_number - 1)
+                    / (factorial(atoms_number - (n + 2)) * factorial(n + 1))
+                )
+            )
         list_sign.append(1)
+
     return list_sign
 
+
 if __name__ == "__main__":
-    atoms_number: int = int(input("what is the atoms number? "))
+    atoms_number = int(input("what is the equivalent atoms number? "))
     sign_number = spin_spin_sign(atoms_number)
-    print(f"The number of signes produce by {atoms_number} equivalent atoms is: {len(sign_number)}")
+    print(
+        f"The number of signes produce by {atoms_number} equivalent atoms is: {len(sign_number)}"
+    )
     print(f"The relation of intesities among signed is: {sign_number}")
